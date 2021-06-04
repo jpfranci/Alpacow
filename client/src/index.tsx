13c349c3c053d569a7d57b92d746e436d2c2e837
jp <jpbfrancisco1998@gmail.com>
2021-06-03 20:22:18 -0700

    setup some dependencies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
