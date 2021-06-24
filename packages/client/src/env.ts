020653ca86127a7e030c90df76e633f398e59c03
Sarah Li <sarahli99@hotmail.com>
2021-06-23 22:46:39 -0700

    remove api key (regen'd)

--- packages/client/src/env.ts
+++ packages/client/src/env.ts
@@ -3,5 +3,5 @@
  * so I settled for this
  *                                                             - Sarah
  */
-const REACT_APP_GOOGLE_API_KEY = `AIzaSyCuMyP16SpYwEjQGrW8feXpW6s2TDv8eks`;
+const REACT_APP_GOOGLE_API_KEY = `YOUR_API_KEY`;
 export default REACT_APP_GOOGLE_API_KEY;
