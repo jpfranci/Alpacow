2a696a965811acab802f278ffd259eb7d7613d65
jp <jpbfrancisco1998@gmail.com>
2021-07-09 22:29:21 -0700

    finish datagen

--- packages/server/setup/datagen.js
+++ packages/server/setup/datagen.js
@@ -7,12 +7,27 @@ const { ObjectID } = require("mongodb");
 const chance = new Chance();
 
 class DataGenerator {
-  constructor({ postsPath, locationPath, commentsPath, daysAgo, numUsers }) {
+  constructor({
+    postsPath,
+    locationPath,
+    commentsPath,
+    tagsPath,
+    daysAgo,
+    numUsers,
+    maxComments,
+  }) {
+    if (numUsers < 3) {
+      throw Error("Number of users to generate must at least be 3");
+    }
     this.postsPath = postsPath;
     this.locationPath = locationPath;
     this.commentsPath = commentsPath;
+    this.tagsPath = tagsPath;
     this.numUsers = numUsers;
+    this.maxComments = Math.min(numUsers, maxComments);
     this.users = [];
+    this.upvotes = [];
+    this.downvotes = [];
     this.setDayRange(daysAgo);
   }
 
@@ -29,20 +44,114 @@ class DataGenerator {
     return chance.integer({ min: start, max: end });
   }
 
-  generateRandomDate() {
-    const randomMillis = this.generateIntInRange(
-      this.startTime,
-      this.currentTime,
-    );
+  getRandomElementFromArray(arr) {
+    const index = chance.integer({ min: 0, max: arr.length - 1 });
+    return {
+      index,
+      element: arr[index],
+    };
+  }
+
+  generateRandomDate(startTime) {
+    const randomMillis = chance.integer({
+      min: startTime,
+      max: this.currentTime,
+    });
     return new Date(randomMillis);
   }
 
   generateRandomLocation() {
-    const index = this.generateIntInRange(0, this.locations.length - 1);
-    return this.locations[index];
+    return this.getRandomElementFromArray(this.mockLocations).element;
   }
 
-  generateVotes() {}
+  // Generate votes by sampling the pool of voters without replacement
+  generateVoters(eligibleVoters, mediaId, numVotes, votesArray) {
+    for (let i = 0; i < numVotes; i++) {
+      const { index: voterIndex } =
+        this.getRandomElementFromArray(eligibleVoters);
+      votesArray.push({
+        userId: eligibleVoters[voterIndex]._id,
+        mediaId,
+      });
+
+      eligibleVoters.splice(voterIndex, 1);
+    }
+  }
+
+  generateVotes(score, id) {
+    let numUpvotes;
+    let numDownvotes;
+    // Can't have more votes than number of users we have generated
+    const scoreToUse = Math.min(
+      Math.floor(Math.abs(Number(score))),
+      Math.floor(this.users.length / 3),
+    );
+    if (scoreToUse > 0) {
+      numUpvotes = chance.integer({
+        min: scoreToUse,
+        max: scoreToUse * 2,
+      });
+      numDownvotes = numUpvotes - scoreToUse;
+    } else {
+      numDownvotes = chance.integer({
+        min: scoreToUse,
+        max: scoreToUse * 2,
+      });
+      numUpvotes = numDownvotes + scoreToUse;
+    }
+
+    const voters = [...this.users];
+    this.generateVoters(voters, id, numUpvotes, this.upvotes);
+    this.generateVoters(voters, id, numDownvotes, this.downvotes);
+    return {
+      numUpvotes: numUpvotes,
+      numDownvotes: numDownvotes,
+    };
+  }
+
+  generateCommentsForPost(postObject) {
+    postObject.comments = [];
+    const numComments = this.generateIntInRange(0, this.maxComments);
+    for (let i = 0; i < numComments; i++) {
+      const commentId = new ObjectID();
+      const { element: commentToUse } = this.getRandomElementFromArray(
+        this.mockComments,
+      );
+      const { body, score } = commentToUse;
+      const date = this.generateRandomDate(postObject.date.getTime());
+      this.generateVotes(score, commentId);
+
+      postObject.comments.push({
+        _id: new ObjectID(),
+        body,
+        date,
+      });
+    }
+  }
+
+  generateDataForPost(post) {
+    const { title, score, body } = post;
+    const date = this.generateRandomDate(this.startTime);
+    const location = this.generateRandomLocation();
+    const id = new ObjectID();
+    const { element: tag } = this.getRandomElementFromArray(this.mockTags);
+    const { numUpvotes, numDownvotes } = this.generateVotes(score, id);
+
+    const postObject = {
+      _id: id,
+      title,
+      numUpvotes,
+      numDownvotes,
+      body: body
+        ? body
+        : this.getRandomElementFromArray(this.mockComments).element.body,
+      date,
+      location,
+      tag,
+    };
+    this.generateCommentsForPost(postObject);
+    return postObject;
+  }
 
   loadPostsCSV() {
     return new Promise((resolve) => {
@@ -50,18 +159,7 @@ class DataGenerator {
       fs.createReadStream(this.postsPath)
         .pipe(csvParser())
         .on("data", (post) => {
-          const { title, score, body } = post;
-          const date = this.generateRandomDate();
-          const location = this.generateRandomLocation();
-          const id = new ObjectID();
-          results.push({
-            _id: id,
-            title,
-            score,
-            body: body ? body : chance.sentence({ words: 5 }),
-            date,
-            location,
-          });
+          results.push(this.generateDataForPost(post));
         })
         .on("end", () => resolve(results));
     });
@@ -69,9 +167,11 @@ class DataGenerator {
 
   loadJSONData() {
     const rawLocations = fs.readFileSync(this.locationPath);
-    this.locations = JSON.parse(rawLocations);
+    this.mockLocations = JSON.parse(rawLocations);
     const rawComments = fs.readFileSync(this.commentsPath);
-    this.comments = JSON.parse(rawComments);
+    this.mockComments = JSON.parse(rawComments);
+    const rawTags = fs.readFileSync(this.tagsPath);
+    this.mockTags = JSON.parse(rawTags);
   }
 
   generateUsers() {
@@ -88,7 +188,6 @@ class DataGenerator {
     this.loadJSONData();
     this.generateUsers();
     const mockPosts = await this.loadPostsCSV();
-    console.log(mockPosts[0]);
   }
 }
 
@@ -110,16 +209,27 @@ parser.add_argument("-c", "--comments", {
   help: "The path to the json file containing mock comments data.",
   dest: "commentsPath",
 });
+parser.add_argument("-t", "--tags", {
+  default: "mock-data/tags_bank.json",
+  help: "The path to the json file containing mock tags data.",
+  dest: "tagsPath",
+});
 parser.add_argument("-d", "--days", {
   default: 14,
   help: `The number of days back from today to start generating data for`,
-  dest: "days",
+  dest: "daysAgo",
   type: "int",
 });
 parser.add_argument("-u", "--num-users", {
   default: 100,
   help: `The number of users to generate.`,
   dest: "numUsers",
+  type: "int",
+});
+parser.add_argument("--max-comments", {
+  default: 50,
+  help: `The maximum number of comments to generate per post`,
+  dest: "maxComments",
   type: "int",
 });
 
@@ -129,8 +239,10 @@ new DataGenerator({
   postsPath: args.postsPath,
   locationPath: args.locationPath,
   commentsPath: args.commentsPath,
-  days: args.days,
+  tagsPath: args.tagsPath,
+  daysAgo: args.daysAgo,
   numUsers: args.numUsers,
+  maxComments: args.maxComments,
 })
   .generate()
   .then(() => {
