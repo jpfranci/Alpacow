e53b77cc6c2619dc18480552b847930a022acf84
Sarah Li <sarahli99@hotmail.com>
2021-07-13 00:56:49 -0700

    cleanup

--- packages/server/src/data/db/db-operations.js
+++ packages/server/src/data/db/db-operations.js
@@ -3,7 +3,7 @@ const Post = require("../models/post-model");
 // We don't want to send all 1000 posts to the client while we have no filters,
 // so this will just sample randomly from all posts
 const getPosts = async () => {
-  return await Post.aggregate([{ $sample: { size: 10 } }]);
+  return await Post.aggregate([{ $sample: { size: 50 } }]);
 };
 
 const operations = {
