2b14713dc175075a311af9b63449560131a89a57
Sarah Li <sarahli99@hotmail.com>
2021-07-16 02:18:46 -0700

    backend add text moderation

--- packages/server/src/data/db/db-operations/post-ops.js
+++ packages/server/src/data/db/db-operations/post-ops.js
@@ -1,4 +1,43 @@
+require("dotenv").config();
 const Post = require("../../models/post-model");
+const axios = require("axios");
+
+const callAzureApi = async (post) => {
+  const headers = {
+    "Content-Type": "text/plain",
+    "Ocp-Apim-Subscription-Key": `${process.env.AZURE_API_KEY}`,
+  };
+  try {
+    return await axios.post(
+      "https://alpacow.cognitiveservices.azure.com/contentmoderator/moderate/v1.0/ProcessText/Screen?autocorrect=True&PII=True&classify=True&language=eng",
+      `${post}`,
+      { headers: headers },
+    );
+  } catch (err) {
+    console.log(err.message);
+    return {
+      data: {
+        Classification: {
+          ReviewRecommended: true,
+        },
+      },
+    };
+  }
+};
+
+const checkIsMature = async (title, body) => {
+  let isMature = false;
+
+  const titleModResponse = await callAzureApi(title);
+  isMature = isMature || titleModResponse.data.Classification.ReviewRecommended;
+  await setTimeout(() => {}, 2000);
+
+  const bodyModResponse = await callAzureApi(body);
+  isMature = isMature || bodyModResponse.data.Classification.ReviewRecommended;
+  await setTimeout(() => {}, 2000);
+
+  return isMature;
+};
 
 // We don't want to send all 1000 posts to the client while we have no filters,
 // so this will just sample randomly from all posts
@@ -6,7 +45,7 @@ const getPosts = async () => {
   return await Post.aggregate([{ $sample: { size: 50 } }]);
 };
 
-const getPostsByFilter = async (lon, lat) => {
+const getPostsByFilter = async (lon, lat, isMature) => {
   return await Post.find({
     location: {
       $near: {
@@ -18,12 +57,35 @@ const getPostsByFilter = async (lon, lat) => {
         $maxDistance: 750,
       },
     },
+    isMature: isMature,
   });
+};
+
+const createPost = async (title, body, tag, location, userId) => {
+  const isMature = await checkIsMature(title, body);
+  const post = new Post({
+    title: title,
+    body: body,
+    tag: tag,
+    location: {
+      type: "Point",
+      coordinates: [location.lon, location.lat],
+    },
+    userId: userId,
+    numUpvotes: 0,
+    numDownvotes: 0,
+    date: Date.now(),
+    comments: [],
+    isMature: isMature,
+  });
+  post.save();
+  return post;
 };
 
 const operations = {
   getPosts,
   getPostsByFilter,
+  createPost,
 };
 
 module.exports = operations;
--- packages/server/src/routes/api/posts.js
+++ packages/server/src/routes/api/posts.js
@@ -10,11 +10,27 @@ router.get("/", async function (req, res, next) {
       posts = await operations.getPostsByFilter(
         locationFilter.lon,
         locationFilter.lat,
+        req.query.matureFilter,
       );
     } else {
       posts = await operations.getPosts();
     }
     res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    const response = await operations.createPost(
+      req.body.params.title,
+      req.body.params.body,
+      req.body.params.tag,
+      req.body.params.location,
+      req.body.params.userId,
+    );
+    res.json(response);
   } catch (err) {
     next(err);
   }
