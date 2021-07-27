533fece5cb04f06bfb64d9de60c49304127a43f4
jp <jpbfrancisco1998@gmail.com>
2021-07-27 03:45:49 -0700

    add index to user schema

--- packages/server/src/data/schemas/user-schema.js
+++ packages/server/src/data/schemas/user-schema.js
@@ -2,7 +2,7 @@ const { Schema } = require("mongoose");
 
 const UserSchema = new Schema({
   _id: String,
-  username: { type: String, unique: true },
+  username: { type: String, index: true, unique: true },
   email: { type: String, index: true, unique: true },
   reputation: Number,
 });
