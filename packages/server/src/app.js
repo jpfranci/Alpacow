b4db129005188b72fcb884da647894c70b189929
Daniel Ryu <rujjiryu99@gmail.com>
2021-07-30 23:33:13 -0700

    pull firebase secrets from env file instead of secret json

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -10,8 +10,21 @@ const apiRouter = require("./routes/api/api-router");
 const admin = require("firebase-admin");
 const { HttpError } = require("./errors/http-error");
 
+// init firebase
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // need this b/c new lines in env variables don't play nice
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  }),
 });
 
 getDb()
